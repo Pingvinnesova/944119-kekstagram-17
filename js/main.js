1ee34434373841135fc68d64dfea4ad975915658
Kate <ekaterina.mihnevich@mail.ru>
2019-06-22 14:13:04 +0300

    Личный проект: пока все дома. Ещё одна попытка)

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
 'use strict';
 
 var USERS_COMMENTS = ['Всё отлично', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-var USERS_NAMES = [''];
+var USERS_NAMES = ['Артём'];
 
-var picture = document.querySelector('#picture');
+var pictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
 
-var photoDescription = [];
+var picturesList = document.querySelector('.pictures');
 
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
+var photo = [];
+
 for (var i = 0; i < 25; i++) {
-  photoDescription[i] = {
+  photo[i] = {
     url: 'photos/' + i + '.jpg',
     likes: getRandomNumber(15, 200),
     comments: {
@@ -21,21 +25,26 @@ for (var i = 0; i < 25; i++) {
       name: USERS_NAMES[Math.floor(Math.random() * USERS_NAMES.length)]
     }
   };
+
+  var photoElement = photo[i];
+
 }
 
-var renderPicture = function () {
-  var pictureElement = picture.cloneNode(true);
+var renderPhoto = function () {
+  var pictureElement = pictureTemplate.cloneNode(true);
 
-  pictureElement.querySelector('.picture__img src').textContent = photoDescription.url;
-  pictureElement.querySelector('.picture__likes').textContent = photoDescription.likes;
-  pictureElement.querySelector('.picture__comments').textContent = photoDescription.likes;
+  pictureElement.querySelector('.picture__img').src = photoElement.url;
+  pictureElement.querySelector('.picture__likes').textContent = photoElement.likes;
+  pictureElement.querySelector('.picture__comments').textContent = photoElement.comments;
 
   return pictureElement;
 };
 
+
 var fragment = document.createDocumentFragment();
-for (var j = 0; j < photoDescription.length; j++) {
-  fragment.appendChild(renderPicture(photoDescription[i]));
+
+for (var j = 0; j < photo.length; j++) {
+  fragment.appendChild(renderPhoto(photo));
 }
 
-picture.appendChild(fragment);
+picturesList.appendChild(fragment);
