f4936ccb870553db55b2610517a2984e6b10747d
Kate <ekaterina.mihnevich@mail.ru>
2019-08-04 14:39:25 +0300

    Правки. Последнее

--- js/set-listeners.js
+++ js/set-listeners.js
@@ -13,25 +13,27 @@
 
   window.setListeners = function () {
     var allPictures = document.querySelectorAll('.picture__img');
-    allPictures.forEach(function (item, index) {
+    allPictures.forEach(function (item) {
       var renderData = function () {
         var commentsCommentShow = 5;
+        var socialCounter = document.querySelector('.comment-counter');
+        socialCounter.textContent = 5;
         var currentPhoto;
         window.id = item.getAttribute('data-id');
         window.savedPhotos.forEach(function (savedPhotoItem) {
-          console.log(savedPhotoItem, window.id);
           if (savedPhotoItem.id === parseFloat(window.id)) {
             currentPhoto = savedPhotoItem;
           }
-        })
+        });
 
         window.openBigPicture();
-        console.log(currentPhoto);
         renderUserData(currentPhoto);
         var commentsLoader = document.querySelector('.comments-loader');
         var socialCommentCount = document.querySelector('.social__comment-count');
+
         commentsLoader.classList.remove('hidden');
         socialCommentCount.classList.remove('hidden');
+
         var commentsLength = currentPhoto.comments.length;
         if (commentsLength <= 5) {
           commentsLoader.classList.add('hidden');
@@ -40,15 +42,19 @@
         var commentsCopy = currentPhoto.comments.slice();
         currentPhoto.comments = currentPhoto.comments.slice(0, commentsCommentShow);
         window.renderComments(currentPhoto);
-        commentsLoader.addEventListener('click', function () {
-          socialCommentCount.classList.add('hidden');
+        // var commentsCount = document.querySelector('.comment-count');
 
+        commentsLoader.addEventListener('click', function () {
+          // socialCommentCount.classList.add('hidden');
           if (commentsCommentShow < commentsLength) {
-            commentsCommentShow += commentsCommentShow;
+
+            commentsCommentShow += 5;
+            socialCounter.textContent = commentsCommentShow;
             currentPhoto.comments = commentsCopy.slice(0, commentsCommentShow);
 
             if (commentsCommentShow >= commentsLength) {
               commentsLoader.classList.add('hidden');
+              socialCommentCount.classList.add('hidden');
             }
           }
           window.renderComments(currentPhoto);
@@ -59,12 +65,15 @@
         renderData();
       });
 
-      // enter на сфокусированную миниатюру
-      item.addEventListener('keydown', function () {
+      var onSmallPictureEnter = function (evt) {
         if (evt.keyCode === window.KeyCode.ENTER_KEYCODE) {
-        renderData();
-      }
-      });
+          renderData();
+        }
+        return onSmallPictureEnter;
+      };
+
+      // enter на сфокусированную миниатюру
+      item.addEventListener('keydown', onSmallPictureEnter);
     });
   };
 })();
